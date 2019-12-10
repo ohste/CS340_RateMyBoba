cd654ca766c1b270a8dbe7b252fe3362bc3c61fd
Stephen Oh <ohste@flip2.engr.oregonstate.edu>
2019-12-10 13:35:16 -0800

    updated header

--- header.php
+++ header.php
@@ -1,27 +1,27 @@
- <?php		
+ <?php
 	$user = $_GET['user'];
 	//$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
 	$content = array(
-		"Add Shop" => "addShop.php",
-		"Add Drink" => "add_drinks.php",
-		"Add Customer" => "addCustomer.php",
-		"Add Manager" => "addManager.php",
+		// "Add Shop" => "addShop.php",
+		// "Add Drink" => "add_drinks.php",
+		// "Add Customer" => "addCustomer.php",
+		// "Add Manager" => "addManager.php",
 		"List Shops" => "listShops.php",
-		"List Drinks" => "list_drinks.php",
+		// "List Drinks" => "list_drinks.php",
 		"List Customers" => "listCustomers.php",
-		"List Managers" => "listManagers.php",                  
-		"Add Ratings" => "addRating.php");
+		// "List Managers" => "listManagers.php",
+		// "Add Ratings" => "addRating.php",
+    "Home" => "home.php");
 ?>
-<header> 
-	BobaShopReview - <em>Welcome <span id="username"><?php echo $user;?></span>!</em>
+<header>
+	<a href="home.php">RATE MY BOBA </a> <div id="bardiv">Welcome <span id="username"><?php echo $user;?></span>!</div>
 </header>
 <nav>
 	<ul>
 	<?php
 		foreach ($content as $page => $location){
-			echo "<li><a href='$location?user=".$user."' ".($page==$currentpage?" class='active'":"").">".$page."</a></li>";			
+			echo "<li><a href='$location?user=".$user."' ".($page==$currentpage?" class='active'":"").">".$page."</a></li>";
 		}
 	?>
 	</ul>
 </nav>
-
