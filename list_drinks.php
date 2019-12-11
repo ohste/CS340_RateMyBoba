a4c8624c8a079eec7079ed1f6dbae04164856a3a
Stephen Oh <ohste@flip1.engr.oregonstate.edu>
2019-12-11 15:25:10 -0800

    Updated css and added linking shops to their specific drinks

--- list_drinks.php
+++ list_drinks.php
@@ -1,6 +1,7 @@
 ﻿<!DOCTYPE html>
 <?php
 		$currentpage="List Drinks";
+		$shop = $_GET['shop'];
 ?>
 <html>
 	<head>
@@ -12,18 +13,20 @@
 
 <?php
 // change the value of $dbuser and $dbpass to your username and password
-	include 'connectvars.php'; 
-	include 'header.php';	
+	include 'connectvars.php';
+	include 'header.php';
 
-
+echo"<div class=\"mainbody\">";
 	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 	if (!$conn) {
 		die('Could not connect: ' . mysql_error());
-	}	
+	}
 
 // query to select all information from supplier table
-	
-	$query = "SELECT * FROM `a.Drink` ";
+
+	$query = "SELECT * FROM Drink, Shop
+		WHERE Drink.ShopID=Shop.ShopID
+		AND Shop.ShopID= $shop";
 
 // Get results from query
 	$result = mysqli_query($conn, $query);
@@ -31,12 +34,12 @@
 		die("Query to show fields from table failed");
 	}
 
-	
-	
+
+
 	if(mysqli_num_rows($result) > 0){
-        echo "<h1>Drinks</h1>";  
+        echo "<h1>Drinks</h1>";
 		echo "<table id='t01' border='1'>";
-        
+
 		echo "<thead>";
 			echo "<tr>";
 			echo "<th>DrinkID</th>";
@@ -46,11 +49,11 @@
 			echo "</tr>";
         echo "</thead>";
         echo "<tbody>";
-		
-        
+
+
 		while($row = mysqli_fetch_array($result)){
-        
-		
+
+
 		    echo "<tr>";
             echo "<td>" . $row['DrinkID'] . "</td>";
 			echo "<td>" . $row['Flavor'] . "</td>";
@@ -58,23 +61,25 @@
 			echo "<td>" . $row['Price'] . "</td>";
             echo "</tr>";
         }
-        echo "</tbody>";                            
+        echo "</tbody>";
         echo "</table>";
 		// Free result set
         mysqli_free_result($result);
     } else{
-		echo "<p class='lead'><em>No records were found.</em></p>";
-    } 
-	
+		echo "<p class='lead'>No drinks yet for this shop! Would you like to</p>";
+    }
+
 	mysqli_close($conn);
+
+
+
+echo "<bottom>";
+echo "<input id=\"addButton\" type=\"Button\" value=\"Add a Drink\" onclick=\"window.location='add_drinks?user=".$user."&shop=".$shop."'\">";
+// echo "Add a drink";
+echo "</input>";
+echo "</bottom>";
+echo "</div>";
 ?>
-
-
-<bottom>
-<input type="button" style="width:200px;height:50px" value="Add a drink" onclick="window.location='add_drinks.php'" />
-
 
 </body>
 </html>
-
-	
