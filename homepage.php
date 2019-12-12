f435e428fdfd0aa2a713797e19b336c0dde8a174
Aeijan Bajracharya <bajracae@flip3.engr.oregonstate.edu>
2019-12-11 22:59:11 -0800

    sexy homepage

--- homepage.php
+++ homepage.php
@@ -9,6 +9,7 @@ $listshop = $_GET['listShops'];
     <title>Home</title>
     <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="animate.css">
 </head>
 
 <body>
@@ -16,13 +17,21 @@ $listshop = $_GET['listShops'];
     <?php
     // change the value of $dbuser and $dbpass to your username and password
     include 'connectvars.php';
-    include 'header.php';
+    // include 'header.php';
 
     ?>
+    <header>
+    <div>
+    <div id = "main"><a href="home.php" id = "home">RATE MY BOBA </a></div>
+    <div id="bardiv"><em>Welcome <span id="username"><?php echo $user;?></span>!</em>
+    </div>
+
+    </header>
     <div class="mainbody">
-        <input type="button" style="width:200px;height:50px" value="See all shops" onclick="window.location='listShops.php'" />
-        <input type="button" style="width:200px;height:50px" value="List customers" onclick="window.location='listCustomers.php'" />
-        <input type="button" style="width:200px;height:50px" value="Sign up" onclick="window.location='addCustomer.php'" />
+        <img class="bobaimg" src="boba.png" alt="Boba Fett">
+        <input class="homebutton" type="button" style="width:200px;height:50px" value="See all shops" onclick="window.location='listShops.php'" />
+        <input class="homebutton" type="button" style="width:200px;height:50px" value="List customers" onclick="window.location='listCustomers.php'" />
+        <input class="homebutton" type="button" style="width:200px;height:50px" value="Sign up" onclick="window.location='addCustomer.php'" />
     </div>
 </body>
 
