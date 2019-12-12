42557d30a7f0d9f528de5421e733086012fe9bc6
Aeijan Bajracharya <bajracae@flip3.engr.oregonstate.edu>
2019-12-11 18:56:30 -0800

    worked on login

--- homepage.php
+++ homepage.php
@@ -1,34 +1,40 @@
 <!DOCTYPE html>
 <?php
-	$currentpage="Home";
-    $listshop=$_GET['listShops'];
+$currentpage = "Home";
+$listshop = $_GET['listShops'];
 ?>
 <html>
-	<head>
-		<title>Home</title>
-		<link rel="stylesheet" href="index.css">
-	</head>
+
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
 <body>
 
-	<?php
-	// change the value of $dbuser and $dbpass to your username and password
-		include 'connectvars.php';
-		include 'header.php';
+    <?php
+    // change the value of $dbuser and $dbpass to your username and password
+    include 'connectvars.php';
+    include 'header.php';
 
-	?>
-<div class="mainbody">
-    <form action="listShops.php" >
-    <input type="submit" value="See all shops">
-    </form>
+    ?>
+    <div class="mainbody">
+        <form action="listShops.php">
+            <input type="submit" value="See all shops">
+        </form>
 
-    <form action="listCustomers.php" >
-    <input type="submit" value="List customers">
-    </form>
+        <form action="listCustomers.php">
+            <input type="submit" value="List customers">
+        </form>
 
-    <form action="addCustomer.php" >
-    <input type="submit" value="Add a customer">
-    </form>
-</div>
-    </body>
+        <form action="addCustomer.php">
+            <input type="submit" value="Add a customer">
+        </form>
 
-	</html>
+        <form action="CustomerSignUpPage.php">
+            <input type="submit" value="Customer Sign Up">
+        </form>
+    </div>
+</body>
+
+</html>
